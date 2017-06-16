5c9a7c91f0faa7637cab2729766ba9b1c2ff3402
bunny <gamefever20@gmal.com>
2017-06-16 22:07:44 +0530

    Added new color theme

--- app/src/main/java/app/calendar/hindu/hinducalendarapp/MainActivity.java
+++ app/src/main/java/app/calendar/hindu/hinducalendarapp/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add Event in calendar", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
